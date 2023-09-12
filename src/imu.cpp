520886cfa0a87e1584f56d6d699ab272da4881c2
pierosimonet <simonetto.piero@gmail.com>
2023-09-12 11:32:21 +0200

    removed the transfomation, need to check

--- src/imu.cpp
+++ src/imu.cpp
@@ -39,7 +39,6 @@ int main(int argc, char** argv){
   current_time = ros::Time::now();
   last_time = ros::Time::now();
 
-
   ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("imu_data", 50);
   ros::Subscriber imu_sub = nh.subscribe("/lidar/lidar_camera/imu", 50, imuCallback);
   tf::TransformBroadcaster imu_broadcaster;
@@ -50,28 +49,28 @@ int main(int argc, char** argv){
     ros::spinOnce();               // check for incoming messages
     current_time = ros::Time::now();
 
-  transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
-  transform.setRotation( tf::Quaternion(0.0,0.0,0.0,1) );
-  imu_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "lidar_camera_link", "imu"));
-  
+    transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+    transform.setRotation( tf::Quaternion(0.0,0.0,0.0,1) );
+    imu_broadcaster.sendTransform(tf::StampedTransform(transform, current_time, "lidar_camera_link", "imu"));
 
-           sensor_msgs::Imu   imu_data;
-           imu_data.header.stamp = current_time;
-           imu_data.header.frame_id = "imu";
+    sensor_msgs::Imu   imu_data;
+    imu_data.header.stamp = current_time;
+    imu_data.header.frame_id = "imu";
 
-            imu_data.orientation.w = w;
-            imu_data.orientation.x = z;
-            imu_data.orientation.y = -x;
-            imu_data.orientation.z = -y;
-            imu_data.linear_acceleration.x = accel_z;
-            imu_data.linear_acceleration.y = -accel_x;
-            imu_data.linear_acceleration.z = -accel_y;
-            imu_data.linear_acceleration_covariance[0] = -1;
-            imu_data.angular_velocity.x = gyro_z;
-            imu_data.angular_velocity.y = -gyro_x;
-            imu_data.angular_velocity.z = -gyro_y;
-            imu_data.angular_velocity_covariance[0] = -1;
-            imu_pub.publish(imu_data);
+    imu_data.orientation.w = w;
+    imu_data.orientation.x = z;
+    imu_data.orientation.y = -x;
+    imu_data.orientation.z = -y;
+    imu_data.linear_acceleration.x = accel_z;
+    imu_data.linear_acceleration.y = -accel_x;
+    imu_data.linear_acceleration.z = -accel_y;
+    imu_data.linear_acceleration_covariance[0] = -1;
+    imu_data.angular_velocity.x = gyro_z;
+    imu_data.angular_velocity.y = -gyro_x;
+    imu_data.angular_velocity.z = -gyro_y;
+    imu_data.angular_velocity_covariance[0] = -1;
+    imu_pub.publish(imu_data);
+    
     last_time = current_time;
     r.sleep();
 }
